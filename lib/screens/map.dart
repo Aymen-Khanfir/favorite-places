3d627766db625621c12dc526d39374c093212f27
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-07-06 12:47:22 +0100

    Displaying the picked place on a dynamic Map and hard coding the API Key

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -7,10 +7,11 @@ class MapScreen extends StatefulWidget {
   const MapScreen({
     super.key,
     this.location = const PlaceLocation(latitude: 37.422, longitude: -122.084, address: ''),
+    this.isSelecting = true,
   });
 
   final PlaceLocation location;
-  final isSelecting = true;
+  final bool isSelecting;
 
   @override
   State<MapScreen> createState() => _MapScreenState();
